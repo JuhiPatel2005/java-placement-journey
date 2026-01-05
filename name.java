f99c88cbfecc1d6efe57473d75a7e21b04c5c800
Juhi Patel <juhipatel2005@gmail.com>
2026-01-05 19:16:44 +0530

    fiexd things

--- name.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class name {
-  public static void main(String args[]){
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter your Name: ");
-    String name = sc.nextLine();
-    System.out.println("Name: " + name);
-  }
-}
